e6a3693e6ee3c27c7162e40284b93b789c07b7ed
Stephen Scott <sscott@ozu.local>
2014-07-14 16:32:22 +1000

    clean up filenames

---
+++ twitter_init.rb
@@ -0,0 +1,6 @@
+$consumer_key = 'xl2qogMKrCcvVjdULSt8EnCxd'
+$consumer_secret = 'FHT78K6QpWtC4mThzkDGBL8sN17QnFPARBj2k8OCLw3wPh3wWa'
+$access_token = '1450454588-sOvX1jbKGHtHCqBq8K63HdoLhFtyB1zvoh0j0x7'
+$access_token_secret = 'MKxOoFyP1q9mDBjCbyqJExiWzKZ6SymFCCRW4qU0MFXJ9'
+
+$source_account = 'stphn'
---
+++ variables.rb
@@ -0,0 +1,9 @@
+# run 1 out of every $rand_limit times roughly (default is 10)
+$rand_limit = 6
+#
+# (default is 2, 1 is more incoherent, 3 is more lucid)
+$markov_index = 3
+#
+# defaults are false
+$include_urls = true
+$include_replies = true
