6bb717c2106a451cc716e13080e7b18e52c9175a
stephen <macbookair.local>
2014-07-20 11:13:11 +1000

    ok

--- generate.rb
+++ generate.rb
@@ -32,7 +32,25 @@ end
 
 def filtered_tweets(tweets)
   html_decoder = HTMLEntities.new
-  source_tweets = tweets.map {|t| html_decoder.decode(t.text) }
+  include_urls = $include_urls || params["include_urls"]
+  include_replies = $include_replies || params["include_replies"]
+  source_tweets = tweets.map {|t| html_decoder.decode(t.text).gsub(/\b(RT|MT) .+/, '') }
+
+  if !include_urls
+    source_tweets = source_tweets.reject {|t| t =~ /(https?:\/\/)/ }
+  end
+
+  if !include_replies
+    source_tweets = source_tweets.reject {|t| t =~ /^@/ }
+  end
+
+  source_tweets.each do |t| 
+    t.gsub!(/(\#|(h\/t)|(http))\S+/, '')
+    t.gsub!(/(@[\d\w_]+\s?)+/, '')
+    t.strip!
+    t << "." if t !~ /[.,?;:!'")]$/
+  end
+
   source_tweets
 end
 
@@ -45,20 +63,20 @@ end
 
   # Fetch a thousand tweets
   begin
-    user_tweets = client.user_timeline($source_account, :count => 200, :trim_user => true, :include_rts => false)
+    user_tweets = client.user_timeline($source_account, :count => 20, :trim_user => true, :include_rts => false)
     max_id = user_tweets.last.id
     source_tweets += filtered_tweets(user_tweets)
   
     # Twitter only returns up to 3200 of a user timeline, includes retweets.
-    17.times do
-      user_tweets = client.user_timeline($source_account, :count => 200, :trim_user => true, :include_rts => false, :max_id => max_id - 1)
-      puts "MAX_ID #{max_id} TWEETS: #{user_tweets.length}"
-      break if user_tweets.last.nil?
-      max_id = user_tweets.last.id
-      source_tweets += filtered_tweets(user_tweets)
-    end
-  rescue => ex
-    puts ex.message
+#    17.times do
+#      user_tweets = client.user_timeline($source_account, :count => 200, :trim_user => true, :include_rts => false, :max_id => max_id - 1)
+#      puts "MAX_ID #{max_id} TWEETS: #{user_tweets.length}"
+#      break if user_tweets.last.nil?
+#      max_id = user_tweets.last.id
+#      source_tweets += filtered_tweets(user_tweets)
+#    end
+#  rescue => ex
+#    puts ex.message
   end
   
   puts "#{source_tweets.length} tweets found"
@@ -68,6 +86,7 @@ end
   end
   
 source_tweets.each do |t|
-  data = { "tweet" => t}
-  Tweet.create!(data)
+puts t
+#  data = { "tweet" => t}
+#  Tweet.create!(data)
 end
