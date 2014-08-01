1b155ee7b03904e9c32913f01f836b8cadfdbeae
Stephen <sscott@ozu.local>
2014-08-01 16:56:44 +1000

    almost there

--- ebook.rb
+++ ebook.rb
@@ -8,6 +8,73 @@ require 'variables'
 require 'markov'
 require 'htmlentities'
 require 'uri'
+require 'mongo'
+
+include Mongo
+
+class Tweet
+  def self.create!(tweets)
+    collection.insert(tweets)
+  end
+
+  def self.fetch
+# get all rows {}. dont get _id field .get tweet field. map to ruby hash from mongo hash. map the tweet key values
+    cursor = collection.find({},{:fields => {"_id" => 0, "text" => 1}}).map { |h| h["text"] }
+  end
+
+  private
+    def self.establish_connection
+     uri = $uri
+     client = Mongo::MongoClient.from_uri($uri)
+     db_name = uri[%r{/([^/\?]+)(\?|$)}, 1]
+     db = client.db(db_name)
+    end
+
+    def self.db
+      @db ||= establish_connection
+    end
+
+    def self.collection
+      @collection ||= db.collection("tweets")
+    end
+end
+
+def filtered_tweets(tweets)
+  html_decoder = HTMLEntities.new
+  include_urls = $include_urls || params["include_urls"]
+  include_replies = $include_replies || params["include_replies"]
+  source_tweets = tweets.map {|t| html_decoder.decode(t).gsub(/\b(RT|MT) .+/, '') }
+
+  if !include_urls
+    source_tweets = source_tweets.reject {|t| t =~ /(https?:\/\/)/ }
+  end
+
+  if !include_replies
+    source_tweets = source_tweets.reject {|t| t =~ /^@/ }
+  end
+
+  source_tweets.each do |t| 
+#    t.gsub!(/(\#|(h\/t)|(http))\S+/, '')
+    t.gsub!(/(@[\d\w_]+\s?)+/, '')
+    t.gsub!(/[”“]/, '"')
+    t.gsub!(/[‘’]/, "'")
+    t.strip!
+  end
+
+  source_tweets
+end
+
+CLOSING_PUNCTUATION = ['.', ';', ':', '?', '!', ',']
+
+def random_closing_punctuation
+  CLOSING_PUNCTUATION[rand(CLOSING_PUNCTUATION.length)]
+end
+
+HASHTAG = ['#discuss', '#change', '#strategy', '#power', '#politics', '#art' , '#repetition', '#conceptual', '#slow', '#zen', '#future', '#oblique']
+
+def random_hashtag
+  HASHTAG[rand(HASHTAG.length)]
+end
 
 source_tweets = []
 
@@ -24,43 +91,6 @@ end
 
 rand_key = rand($rand_limit)
 
-CLOSING_PUNCTUATION = ['.', ';', ':', '?', '!', ',']
-
-def random_closing_punctuation
-  CLOSING_PUNCTUATION[rand(CLOSING_PUNCTUATION.length)]
-end
-
-HASHTAG = ['#discuss', '#change', '#strategy', '#power', '#politics', '#art' , '#repetition', '#conceptual', '#slow', '#zen', '#future', '#oblique']
-
-def random_hashtag
-  HASHTAG[rand(HASHTAG.length)]
-end
-
-def filtered_tweets(tweets)
-  html_decoder = HTMLEntities.new
-  include_urls = $include_urls || params["include_urls"]
-  include_replies = $include_replies || params["include_replies"]
-  source_tweets = tweets.map {|t| html_decoder.decode(t.text).gsub(/\b(RT|MT) .+/, '') }
-
-  if !include_urls
-    source_tweets = source_tweets.reject {|t| t =~ /(https?:\/\/)/ }
-  end
-
-  if !include_replies
-    source_tweets = source_tweets.reject {|t| t =~ /^@/ }
-  end
-
-  source_tweets.each do |t| 
-#    strip out twitter handles
-    t.gsub!(/(@[\d\w_]+\s?)+/, '')
-    t.gsub!(/[”“]/, '"')
-    t.gsub!(/[‘’]/, "'")
-    t.strip!
-  end
-
-  source_tweets
-end
-
 # randomly running only about 1 in $rand_limit times
 unless rand_key == 0 || params["force"]
   puts "Not running this time (key: #{rand_key})"
@@ -73,29 +103,14 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = $access_token_secret
 end
 
-  # Fetch a thousand tweets
-  begin
-    user_tweets = client.user_timeline($source_account, :count => 200, :trim_user => true)
-    max_id = user_tweets.last.id
-    source_tweets += filtered_tweets(user_tweets)
+user_tweets = Tweet.fetch
+source_tweets += filtered_tweets(user_tweets)
   
-    # Twitter only returns up to 3200 of a user timeline, includes retweets.
-    17.times do
-      user_tweets = client.user_timeline($source_account, :count => 200, :trim_user => true, :max_id => max_id - 1)
-      puts "MAX_ID #{max_id} TWEETS: #{user_tweets.length}"
-      break if user_tweets.last.nil?
-      max_id = user_tweets.last.id
-      source_tweets += filtered_tweets(user_tweets)
-    end
-  rescue => ex
-    puts ex.message
-  end
-  
-  puts "#{source_tweets.length} tweets found"
+puts "#{source_tweets.length} tweets found"
 
-  if source_tweets.length == 0
-    raise "Error fetching tweets from Twitter. Aborting."
-  end
+if source_tweets.length == 0
+  raise "Error fetching tweets from Twitter. Aborting."
+end
   
   markov = MarkovChainer.new($markov_index)
 
@@ -104,15 +119,6 @@ end
   source_tweets.each do |twt|
     next if twt.nil? || twt == ''
     sentences = tokenizer.sentences_from_text(twt, :output => :sentences_text)
-
-    # sentences = text.split(/[.:;?!]/)
-
-    # sentences.each do |sentence|
-    #   next if sentence =~ /@/
-
-    #   if sentence !~ /\p{Punct}$/
-    #     sentence += "."
-    #   end
 
     sentences.each do |sentence|
       next if sentence =~ /@/
@@ -127,11 +133,6 @@ end
 
     tweet_letters = tweet.gsub(/\P{Word}/, '')
     next if source_tweets.any? {|t| t.gsub(/\P{Word}/, '') =~ /#{tweet_letters}/ }
-
-    # if rand(3) == 0 && tweet =~ /(in|to|from|for|with|by|our|of|your|around|under|beyond)\p{Space}\w+$/ 
-    #   puts "Losing last word randomly"
-    #   tweet.gsub(/\p{Space}\p{Word}+.$/, '')   # randomly losing the last word sometimes like horse_ebooks
-    # end
 
     if tweet.length < 40 && rand(10) == 0
       puts "Short tweet. Adding another sentence randomly"
