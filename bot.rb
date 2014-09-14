a6497eb72d9ce0e0127dcc755c171592c8907b80
stephen <macbookair.local>
2014-09-14 14:43:23 +1000

    fix line that strips out urls

--- bot.rb
+++ bot.rb
@@ -166,7 +166,7 @@ end
   tweet.gsub!(/\p{Punct}$/, '') if tweet =~ URI::regexp
 
 # strip out the url
-#  tweet.gsub!(/http:\/\/.+\//, '')
+  tweet.gsub!(/http:\/\/.+/, '')
 
 # add a random hashtag for 1 in 3 tweets and if the tweet is less than 125 chars
   tweet += " #{random_hashtag}" if rand(2) == 0 && tweet.length < 125
